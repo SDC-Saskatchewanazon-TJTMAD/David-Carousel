cfce1652bf0d59defb0a52383baa73328a2640fd
David M <d.anthony.massey@gmail.com>
2020-02-19 17:06:32 -0600

    Connect component to posgresDB

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import StarRatingComponent from 'react-star-rating-component';
+import React from "react";
+import StarRatingComponent from "react-star-rating-component";
 
 export default function Product(props) {
   return (
-    <div className='productContainer'>
+    <div className="productContainer">
       {Array.from(props.productsFive).map((product, index) => {
-        return(
-          <div key={index} className='productBlock'>
-            <img className='image' src={product.imgUrl} id={product.productId} data-cat={product.categoryId} onClick={props.goToProduct}></img>
-            <div className='textBox'>
-              <div className='nameText' id={product.productId} onClick={props.goToProduct}>{product.productName}</div>
-                <div>
-                  <StarRatingComponent
-                    name={product.productId.toString()}
-                    editing={false}
-                    renderStarIcon={() => <span id={product.productId} data-cat={product.categoryId} onClick={props.goToRating} className='rating'>🍁</span>}
-                    starCount={Math.round(product.rating)}
-                  />
-                </div>
-              <div className='priceText' id={product.productId} onClick={props.goToProduct}>${(product.price).toFixed(2)}</div>
+        return (
+          <div key={index} className="productBlock">
+            <img
+              className="image"
+              src={product.imgUrl}
+              id={product.productId}
+              data-cat={product.categoryId}
+              onClick={props.goToProduct}
+            ></img>
+            <div className="textBox">
+              <div
+                className="nameText"
+                id={product.productId}
+                onClick={props.goToProduct}
+              >
+                {product.productName}
+              </div>
+              <div>
+                <StarRatingComponent
+                  name="p"
+                  editing={false}
+                  renderStarIcon={() => (
+                    <span>
+                      <img
+                        src="./images/half_leaf.png"
+                        alt="test"
+                        width="40"
+                        height="45"
+                      />
+                    </span>
+                  )}
+                  starCount={Math.round(product.rating)}
+                />
+              </div>
+              <div
+                className="priceText"
+                id={product.productId}
+                onClick={props.goToProduct}
+              >
+                ${product.price.toFixed(2)}
+              </div>
             </div>
           </div>
-        )
+        );
       })}
     </div>
   );
--- db/query.js
+++ db/query.js
@@ -1,56 +1,58 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'saskatchewanizon'
-});
- 
-connection.connect();
- 
-const getProducts = (categoryId, callback) => {
-  if (categoryId == '0') {
-    connection.query(`SELECT * FROM itemData;`, (err, data) => {
-      if (err) {
-        callback(err, null)
-      } else {
-        callback(null, data)
-      }
-    })
+var promise = require("bluebird");
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require("pg-promise")(options);
+var db = pgp("postgres://postgres:postgres@localhost:5432/tenmill");
+
+const getProducts = categoryId => {
+  console.log(`this is the category ID ${categoryId}`);
+  if (categoryId == 0) {
+    db.any(`SELECT * FROM product_info LIMIT 100;`)
+      .then(data => {
+        console.log("DATA:", data);
+      })
+      .catch(function(error) {
+        console.log("ERROR:", error);
+      });
   } else {
-    connection.query(`SELECT * FROM itemData WHERE categoryID = ('${categoryId}');`, (err, data) => {
-      if (err) {
-        callback(err, null)
-      } else {
-        callback(null, data)
-      }
-    })
+    db.any(
+      `SELECT * FROM product_info WHERE category IN (${categoryId}) LIMIT 100;`
+    )
+      .then(function(data) {
+        console.log("DATA:", data.value);
+      })
+      .catch(function(error) {
+        console.log("ERROR:", error);
+      });
   }
-}
+};
 
-// const createTask = (item, callback) => {
-//   connection.query(`INSERT INTO tasks (task) VALUES ('${item}');`, (err, data) => {
-//     if (err) {
-//       callback(err, null)
-//     } else {
-//       callback(null, data)
-//     }
-//   })
+//   if (categoryId == '0') {
+//     connection.query(`SELECT * FROM itemData;`, (err, data) => {
+//       if (err) {
+//         callback(err, null)
+//       } else {
+//         callback(null, data)
+//       }
+//     })
+//   } else {
+//     connection.query(`SELECT * FROM itemData WHERE categoryID = ('${categoryId}');`, (err, data) => {
+//       if (err) {
+//         callback(err, null)
+//       } else {
+//         callback(null, data)
+//       }
+//     })
+//   }
 // }
 
-// const deleteTask = (item, callback) => {
-//   connection.query(`DELETE FROM tasks WHERE id=('${item}');`, (err, data) => {
-//     if (err) {
-//       callback(err, null)
-//     } else {
-//       callback(null, data)
-//     }
-//   })
-// }
+// connection.query('SELECT 1 + 1 AS solution', function (error, results) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
-
-module.exports = { getProducts }
+module.exports = { getProducts };
